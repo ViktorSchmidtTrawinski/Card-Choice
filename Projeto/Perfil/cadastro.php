4cbda50cd2c39b5fbf2b8ca4f7ef8a53cd66117d
ViktorSchmidtTrawinski <vi.trawinski@gmail.com>
2026-09-11 14:07:42 -0300

    Implementação do cadastro de usuário com validação de email e hash de senha

--- Projeto/Perfil/cadastro.php
+++ Projeto/Perfil/cadastro.php
@@ -1,4 +1,48 @@
-<?php  require "../connect.php"?>
+<?php
+require_once "../connect.php";
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    header('Content-Type: application/json');
+    $nome = trim($_POST['nome']);
+    $email = trim($_POST['email']);
+    $senha = trim($_POST['senha']);
+
+    if(!empty($nome)&&!empty($email)&&!empty($senha)){
+        if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+            try {
+                $sql = 'SELECT * FROM conta WHERE email = :email';
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute([':email' => $email]);
+                if($stmt->rowCount()>0){
+                    echo json_encode(['status'=>'erro','mensagem'=>'conta já cadastrada']);
+                    exit;
+                }else{
+                    try{
+                        $senha = password_hash($senha,PASSWORD_DEFAULT);
+                        $sql = 'INSERT INTO conta(nome,email,senha) VALUES (:nome,:email,:senha)';
+                        $stmt = $pdo->prepare($sql);
+                        $stmt->execute([':nome' => $nome,
+                                        ':email'=> $email,
+                                        ':senha'=> $senha]);
+                        echo json_encode(['status'=>'sucesso','mensagem'=>'Usuario cadastrado com sucesso!']);
+                        exit;
+                    }catch(PDOException $e){
+                        echo json_encode(['status'=>'erro','mensagem'=>'Erro ao inserir dados do usuario '. $e->getMessage()]);
+                        exit;
+                    }
+                }
+            }catch(PDOException $e) {
+                echo json_encode(['status'=>'erro','mensagem'=>'Erro ao verificar email do usuario '. $e->getMessage()]);
+                exit;
+            }
+        }else{
+            echo json_encode(['status'=>'erro','mensagem'=>'Email invalido']);
+            exit;
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -6,22 +50,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="../style.css"></link>
+    <script src="../Perfil/cadastro.js" defer></script>
 </head>
-
 <body>
     <header><?php include '../nav.php'?></header>
     <div class="container-cadastro">
         <form id="form-cadastro" method="POST">
         <label for="nome">Nome</label>    
-        <input id="nome" type="text" required>
+        <input name="nome" id="nome" type="text" required>
         <label for="email">Email</label>    
-        <input id="email" type="email" required>
+        <input name="email" id="email" type="email" required>
         <label for="senha">Senha</label>    
-        <input id="senha" type="password" required>
+        <input name="senha" id="senha" type="password" required>
         <button type="submit">Cadastrar</button>
         <p>Já possui uma conta? <a href="#">Login</a></p>
         </form>
     </div>
-    
 </body>
 </html>
